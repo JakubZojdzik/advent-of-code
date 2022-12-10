7f679411d181f7382a7a44fe08d72afeb97607b4
JakubZojdzik <jakub.zojdzik@gmail.com>
2022-12-10 23:53:12 +0100

    No camel will beat me B)

--- 10/main2.ml
+++ 10/main2.ml
@@ -1,6 +1,20 @@
 let x = ref 1;;
-let res = ref 0;;
 let itr = ref 0;;
+
+let next_iteration () =
+(
+    let tmp = !itr mod 40 in
+    if !x >= tmp-1 && !x <= tmp+1 then
+    (
+        Printf.printf "#";
+    )
+    else
+    (
+        Printf.printf " ";
+    );
+    itr := !itr + 1;
+    if !itr mod 40 = 0 then Printf.printf "\n";
+);;
 
 let () =
     let ic = open_in "inp" in
@@ -8,29 +22,17 @@ let () =
         while true do
             let line = input_line ic in
             if String.sub line 0 4 = "noop" then
-            (
-                itr := !itr + 1;
-                if (!itr + 20) mod 40 = 0 then
-                (
-                    res := !res + !x * !itr;
-                );
-            );
+                next_iteration ();
 
             if String.sub line 0 4 = "addx" then
             (
                 let nw = String.sub line 5 (String.length line - 5) in
                 let nwint = int_of_string nw in
-                for i = 0 to 1 do
-                    itr := !itr + 1;
-                    if (!itr + 20) mod 40 = 0 then
-                    (
-                        res := !res + !x * !itr;
-                    );
-                done;
+                next_iteration ();
+                next_iteration ();
                 x := !x + nwint;
             );
+            (* Printf.printf "x = %d, itr = %d\n" !x !itr; *)
         done
     with End_of_file ->
         close_in ic;
-
-    Printf.printf "RESULT: %d\n" !res
